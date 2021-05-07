60fd64f1bbbb7136e09a7a58333adfd158911c57
RoadtoS7 <kshvv975@gmail.com>
2021-05-07 20:44:41 +0900

    style(MainActivity.kt):view폴더 이름 main으로 변경

---
+++ app/src/main/java/com/engdiary/mureng/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.engdiary.mureng.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.engdiary.mureng.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
