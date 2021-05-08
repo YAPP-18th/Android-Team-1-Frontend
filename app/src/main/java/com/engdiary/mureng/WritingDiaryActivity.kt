ca4704488e04509e097bd9b69cbf29eca73615bb
RoadtoS7 <kshvv975@gmail.com>
2021-05-08 19:20:01 +0900

    feat(WritingDiaryActivity.kt): WritingDiaryActivity 추가
    일기 작성하는 액티비티 추가

---
+++ app/src/main/java/com/engdiary/mureng/WritingDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.engdiary.mureng
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WritingDiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writing_diary)
+    }
+}
