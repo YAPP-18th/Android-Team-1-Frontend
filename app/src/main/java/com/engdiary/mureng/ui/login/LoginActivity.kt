06ba0dd68de2ca2eec705d43f6ec537a4169ca34
qualson mac book <>
2021-05-14 15:35:15 +0900

    add: Login Activity

---
+++ app/src/main/java/com/engdiary/mureng/ui/login/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.engdiary.mureng.ui.login
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.engdiary.mureng.R
+import com.engdiary.mureng.databinding.ActivityLoginBinding
+import com.engdiary.mureng.ui.base.BaseActivity
+import com.engdiary.mureng.ui.main.MainViewModel
+
+
+class LoginActivity: BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val TAG = "LoginActivity"
+
+    override val viewModel: LoginViewModel by viewModels<LoginViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.kakaoLogin.setOnClickListener {
+            Log.i(TAG, "Log i")
+        }
+
+    }
+}
