240dc044b8f5b48b5ae6be041bf2afce661e201e
RoadtoS7 <kshvv975@gmail.com>
2021-05-14 19:48:50 +0900

    refact(MainActivity.kt): MainActivity 폴더 위치 변경

--- app/src/main/java/com/engdiary/mureng/ui/main/MainActivity.kt
+++ app/src/main/java/com/engdiary/mureng/ui/main/MainActivity.kt
@@ -1,21 +1,21 @@
-package com.engdiary.mureng.view.main
+package com.engdiary.mureng.ui.main
 
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.engdiary.mureng.BR
 import com.engdiary.mureng.R
+import com.engdiary.mureng.databinding.ActivityMainBinding
 import com.engdiary.mureng.ui.base.BaseActivity
 import com.engdiary.mureng.ui.base.navigate
 import com.engdiary.mureng.ui.home.HomeFragment
-import com.engdiary.mureng.ui.main.MainViewModel
 import com.engdiary.mureng.ui.my.MyPageFragment
 import com.engdiary.mureng.ui.social.SocialFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<com.engdiary.mureng.databinding.ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override val viewModel: MainViewModel by viewModels<MainViewModel>()
 
