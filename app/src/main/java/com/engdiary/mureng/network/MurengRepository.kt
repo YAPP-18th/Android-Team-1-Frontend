945c75c587c666ef0b73590444f2c97bb0cf8c8a
RoadtoS7 <kshvv975@gmail.com>
2021-05-16 14:53:50 +0900

    feat(MurengRepository.kt): 오늘의 질문 얻어오는 getTodayQuestion() 추가
    getTodayQuestion() 함수는 에러 처리 로직에 따라서 수정될 수 있습니다

--- app/src/main/java/com/engdiary/mureng/network/MurengRepository.kt
+++ app/src/main/java/com/engdiary/mureng/network/MurengRepository.kt
@@ -1,6 +1,8 @@
 package com.engdiary.mureng.network
 
 
+import com.engdiary.mureng.data.ItemWritingDiaryImage
+import com.engdiary.mureng.data.TodayQuestion
 import com.engdiary.mureng.di.AuthManager
 import javax.inject.Inject
 
@@ -21,4 +23,10 @@ class MurengRepository @Inject constructor(
     )
     }
      */
+
+    suspend fun getTodayQuestion(): TodayQuestion? {
+        return api.getTodayQuestion("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsIm5pY2tuYW1lIjoi7YWM7Iqk7Yq47Jyg7KCAIiwiaWF0IjoxNjIwODM4MTAyLCJleHAiOjE5MDAwMDAwMDB9.R9__KIcXK_MWrxc857K5IQpwoPYlEyt4eW52VsaRBDid1aFRqw8Uu_oeoserjFEjeiUmrqpAal5XvllrdNH52Q")
+            .data
+            ?.toDomain()
+    }
 }
