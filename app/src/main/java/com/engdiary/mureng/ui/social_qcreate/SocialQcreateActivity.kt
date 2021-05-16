ac8e95eae40f806778bab9cbbc6290c7e158464b
yunakim2 <98_02@naver.com>
2021-05-16 18:37:53 +0900

    fix(socialQcreateActivity, ViewModel, xml) : SocialQcreateActivity.kt, SocialQcreateViewModel.kt 구현 (정규식 수정 필요)

---
+++ app/src/main/java/com/engdiary/mureng/ui/social_qcreate/SocialQcreateActivity.kt
@@ -0,0 +1,41 @@
+package com.engdiary.mureng.ui.social_qcreate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.engdiary.mureng.BR
+import com.engdiary.mureng.R
+import com.engdiary.mureng.databinding.ActivityMainBinding
+import com.engdiary.mureng.databinding.ActivitySocialQcreateBinding
+import com.engdiary.mureng.ui.base.BaseActivity
+import com.engdiary.mureng.ui.base.navigate
+import com.engdiary.mureng.ui.home.HomeFragment
+import com.engdiary.mureng.ui.main.MainViewModel
+import com.engdiary.mureng.ui.my.MyPageFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+
+@AndroidEntryPoint
+class SocialQcreateActivity : BaseActivity<ActivitySocialQcreateBinding>(R.layout.activity_social_qcreate) {
+
+    override val viewModel: SocialQcreateViewModel by viewModels<SocialQcreateViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.setVariable(BR.vm, viewModel)
+
+        viewModel.backButton.observe(this, Observer {
+            if(it) {
+                finish()
+            }
+        })
+
+        viewModel.registerQues.observe(this, Observer {
+            if(it) {
+                finish()
+            }
+        })
+
+    }
+}
