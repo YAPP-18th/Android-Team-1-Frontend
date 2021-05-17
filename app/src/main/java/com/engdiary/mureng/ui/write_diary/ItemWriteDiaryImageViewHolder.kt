06938843eaabb1c34dde073598670d0376a5e790
RoadtoS7 <kshvv975@gmail.com>
2021-05-18 02:37:38 +0900

    feat(ItemWriteDiaryImageViewHolder.kt): 파일 이름 수정 및 사이즈 화면크기의 3분의 1되도록 구현

---
+++ app/src/main/java/com/engdiary/mureng/ui/write_diary/ItemWriteDiaryImageViewHolder.kt
@@ -0,0 +1,37 @@
+package com.engdiary.mureng.ui.write_diary
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.engdiary.mureng.data.ItemWriteDiaryImage
+import com.engdiary.mureng.databinding.ItemWritingdiaryimageImageBinding
+
+
+class ItemWriteDiaryImageViewHolder private constructor(
+    private val binding: ItemWritingdiaryimageImageBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(
+        itemWriteDiaryImage: ItemWriteDiaryImage.DiaryImage,
+        onClickListener: WriteDiaryImageAdapter.OnClickListener
+    ) {
+        binding.itemWritingDiaryImage = itemWriteDiaryImage
+        binding.onClickListener = onClickListener
+    }
+
+    companion object {
+        const val viewType = 1
+
+        fun from(parent: ViewGroup, itemWidth: Int): ItemWriteDiaryImageViewHolder {
+            return ItemWritingdiaryimageImageBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ).let { ItemWriteDiaryImageViewHolder(it) }
+                .apply {
+                    itemView.layoutParams.height = itemWidth
+                    itemView.layoutParams.width = itemWidth
+                }
+        }
+    }
+}
