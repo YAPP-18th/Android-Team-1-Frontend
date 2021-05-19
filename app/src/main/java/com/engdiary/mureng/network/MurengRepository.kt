e052b49c76954fa8a96706043e12081cad0f2421
RoadtoS7 <kshvv975@gmail.com>
2021-05-20 01:31:55 +0900

    feat: MurengRepository에 서버에서 디폴트 이미지 얻어오는 함수 구현

--- app/src/main/java/com/engdiary/mureng/network/MurengRepository.kt
+++ app/src/main/java/com/engdiary/mureng/network/MurengRepository.kt
@@ -5,17 +5,18 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import com.engdiary.mureng.data.Diary
 import com.engdiary.mureng.data.DiaryContent
 import com.engdiary.mureng.data.ItemWriteDiaryImage
-import com.engdiary.mureng.data.TodayQuestion
+import com.engdiary.mureng.data.Question
 import com.engdiary.mureng.data.request.PostDiaryRequest
-import com.engdiary.mureng.data.response.PostDiaryImageResponse
 import com.engdiary.mureng.di.AuthManager
+import com.engdiary.mureng.di.BASE_URL
 import com.engdiary.mureng.di.MurengApplication
-import com.engdiary.mureng.di.send
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.toRequestBody
+import timber.log.Timber
 import java.io.ByteArrayOutputStream
 import java.io.File
 import javax.inject.Inject
@@ -141,7 +142,26 @@ class MurengRepository @Inject constructor(
                     "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsIm5pY2tuYW1lIjoi7YWM7Iqk7Yq47Jyg7KCAIiwiaWF0IjoxNjIwODM4MTAyLCJleHAiOjE5MDAwMDAwMDB9.R9__KIcXK_MWrxc857K5IQpwoPYlEyt4eW52VsaRBDid1aFRqw8Uu_oeoserjFEjeiUmrqpAal5XvllrdNH52Q",
                     it
                 )
-            }.send()
-        return response.body()?.data?.diaryId
+            }
+
+        if (!response.isSuccessful) {
+            Timber.d("Post Diary Fail (code: ${response.code()}) (message: ${response.message()}) (response: ${response.raw()})")
+        }
+
+        return response.body()?.data?.asDomain()
+    }
+
+    suspend fun getDefaultDiaryImages(): List<ItemWriteDiaryImage.DiaryImage>? {
+        val response =
+            api.getDefaultImages("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsIm5pY2tuYW1lIjoi7YWM7Iqk7Yq47Jyg7KCAIiwiaWF0IjoxNjIwODM4MTAyLCJleHAiOjE5MDAwMDAwMDB9.R9__KIcXK_MWrxc857K5IQpwoPYlEyt4eW52VsaRBDid1aFRqw8Uu_oeoserjFEjeiUmrqpAal5XvllrdNH52Q")
+
+        if (!response.isSuccessful) {
+            Timber.d("Get Default Images Fail (code: ${response.code()}) (message: ${response.message()} (respnse: ${response.raw()})")
+        }
+
+        return response.body()
+            ?.data?.mapIndexed { index, imagePath ->
+                ItemWriteDiaryImage.DiaryImage(index, BASE_URL + imagePath, imagePath)
+            }
     }
 }
