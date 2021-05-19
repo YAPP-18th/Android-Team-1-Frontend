438c4eec228cecb3df9c670fe57b86030bd07605
RoadtoS7 <kshvv975@gmail.com>
2021-05-20 01:22:22 +0900

    feat: DiaryNetwork.kt 작성한 일기에 대한 네트워크 클래스 생성

---
+++ app/src/main/java/com/engdiary/mureng/data/response/DiaryNetwork.kt
@@ -0,0 +1,39 @@
+package com.engdiary.mureng.data.response
+
+import com.engdiary.mureng.data.Diary
+import com.engdiary.mureng.data.DiaryContent
+import com.google.gson.annotations.SerializedName
+
+data class DiaryNetwork(
+    @SerializedName("author")
+    val author: AuthorNetwork,
+    @SerializedName("content")
+    val content: String,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("question")
+    val question: QuestionNetwork,
+    @SerializedName("questionId")
+    val questionId: Int,
+    @SerializedName("regDate")
+    val date: String,
+    @SerializedName("replyId")
+    val id: Int,
+    @SerializedName("replyLikeCount")
+    val likeCount: Int,
+    @SerializedName("requestedByAuthor")
+    val isMine: Boolean
+) {
+    fun asDomain(): Diary =
+        Diary(
+            author.asDomain(),
+            DiaryContent.of(content),
+            image,
+            question.asDomain(),
+            questionId,
+            date,
+            id,
+            likeCount,
+            isMine
+        )
+}
