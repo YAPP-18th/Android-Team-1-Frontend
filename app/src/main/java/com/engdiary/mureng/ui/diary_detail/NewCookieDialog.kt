635031a4617a0d855c4d0f72712cf1f0331277c1
RoadtoS7 <kshvv975@gmail.com>
2021-05-30 03:00:39 +0900

    fix: 새로운 쿠키 획득 다이얼로그 이름 수정
    NewCookieDialog.kt, dialog_new_cookie.xml로 새로운 쿠키 획득 다이얼로그 이름 수정

--- app/src/main/java/com/engdiary/mureng/ui/diary_detail/NewCookieDialog.kt
+++ app/src/main/java/com/engdiary/mureng/ui/diary_detail/NewCookieDialog.kt
@@ -3,14 +3,14 @@ package com.engdiary.mureng.ui.diary_detail
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import com.engdiary.mureng.databinding.DialogDiaryCookieBinding
+import com.engdiary.mureng.databinding.DialogNewCookieBinding
 
-class DiaryCookieDialog(context: Context) : Dialog(context) {
-    private lateinit var binding: DialogDiaryCookieBinding
+class NewCookieDialog(context: Context) : Dialog(context) {
+    private lateinit var binding: DialogNewCookieBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DialogDiaryCookieBinding.inflate(layoutInflater)
+        binding = DialogNewCookieBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.textviewDiarycookieAccept.setOnClickListener { this.dismiss() }
