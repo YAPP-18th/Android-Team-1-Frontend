5113112a36441fd31e5ed7155441a3d3cd0ed586
yunakim2 <98_02@naver.com>
2021-06-19 02:40:47 +0900

    feat : 인스타그램, 오픈소스 라이센스 Webview 연결

---
+++ app/src/main/java/com/engdiary/mureng/constant/URLConstant.kt
@@ -0,0 +1,6 @@
+package com.engdiary.mureng.constant
+
+object URLConstant {
+    const val LISENCE_URL = "https://www.notion.so/mureng/db96a9fadbc449258f887dc90b3b185b"
+    const val INSTAR_GRAM_URL = "https://www.instagram.com/mureng_official/"
+}
