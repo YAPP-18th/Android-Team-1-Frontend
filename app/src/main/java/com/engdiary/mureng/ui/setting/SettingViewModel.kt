f490c42ae7cc41cc0e8e2a6931a3018502311c01
RoadtoS7 <kshvv975@gmail.com>
2021-06-20 02:22:21 +0900

    fix: 설정화면에서 푸시알림 설정화면으로 푸시 설정 정보 넘기도록 수정

---
+++ app/src/main/java/com/engdiary/mureng/ui/setting/SettingViewModel.kt
@@ -0,0 +1,28 @@
+package com.engdiary.mureng.ui.setting
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.engdiary.mureng.data.PushAlertSetting
+import com.engdiary.mureng.data.SingleLiveEvent
+import com.engdiary.mureng.network.MurengRepository
+import com.engdiary.mureng.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(private val murengRepository: MurengRepository) :
+    BaseViewModel(murengRepository) {
+
+    private val _navigateToPushAlert = SingleLiveEvent<PushAlertSetting>()
+    val navigateToPushAlert: LiveData<PushAlertSetting>
+        get() = _navigateToPushAlert
+
+    fun navigateToPushAlert() {
+        viewModelScope.launch(Dispatchers.IO) {
+            murengRepository.getMyInfo()?.pushAlertSetting
+                ?.let { _navigateToPushAlert.postValue(it) }
+        }
+    }
+}
