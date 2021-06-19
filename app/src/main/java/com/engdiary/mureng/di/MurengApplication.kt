dc332423a350cc5054418ec77c4d6bdd126e88b8
RoadtoS7 <kshvv975@gmail.com>
2021-06-19 13:36:09 +0900

    feature: MurengApplication에 채널 생성 구현

--- app/src/main/java/com/engdiary/mureng/di/MurengApplication.kt
+++ app/src/main/java/com/engdiary/mureng/di/MurengApplication.kt
@@ -1,14 +1,19 @@
 package com.engdiary.mureng.di
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import com.engdiary.mureng.R
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class MurengApplication : Application() {
     companion object {
-        var appContext : Context? = null
+        var appContext: Context? = null
 
         fun getGlobalApplicationContext(): Context {
             checkNotNull(appContext) { "This Application does not inherit com.kakao.GlobalApplication" }
@@ -20,11 +25,41 @@ class MurengApplication : Application() {
             return appContext!! as MurengApplication
         }
     }
+
     override fun onCreate() {
         super.onCreate()
         appContext = this
         Timber.plant(
             Timber.DebugTree()
         )
+
+        createChannel(
+            getString(R.string.like_channel_id),
+            getString(R.string.like_channel_name),
+            getString(R.string.like_channel_description)
+        )
+        createChannel(
+            getString(R.string.daily_channel_id),
+            getString(R.string.daily_channel_name),
+            getString(R.string.daily_channel_description)
+        )
+    }
+
+    fun createChannel(channelId: String, channelName: String, channelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply { setShowBadge(false) }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = channelDescription
+
+            this.getSystemService(NotificationManager::class.java)
+                .run { createNotificationChannel(notificationChannel) }
+        }
     }
 }
