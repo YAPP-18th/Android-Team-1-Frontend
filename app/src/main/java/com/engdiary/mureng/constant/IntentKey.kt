05fb6b6b1f3d70424ee60111d451c8960856d953
RoadtoS7 <kshvv975@gmail.com>
2021-06-20 02:19:30 +0900

    feature: IntentKey에 PushAlertSetting 키 추가

--- app/src/main/java/com/engdiary/mureng/constant/IntentKey.kt
+++ app/src/main/java/com/engdiary/mureng/constant/IntentKey.kt
@@ -7,4 +7,5 @@ object IntentKey {
     val EDITED_DIARY = Pair("EDITED_DIARY_KEY", "EDITED_DIARY_VALUE")
     const val OPEN_SOURCE = "OPEN_SOURCE"
     const val QUESTION_NETWORK = "QUESTION_NETWORK"
+    const val PUSH_ALERT_SETTING = "PUSH_ALERT_SETTING"
 }
