9167bafef86062478a49286ec814291ede43d9b8
RoadtoS7 <kshvv975@gmail.com>
2021-06-20 16:59:24 +0900

    fix: MurengFCMService에서 coroutineScope하고 MurengRepository DI 받도록 수정

--- app/src/main/java/com/engdiary/mureng/fcm/MurengFCMService.kt
+++ app/src/main/java/com/engdiary/mureng/fcm/MurengFCMService.kt
@@ -2,11 +2,24 @@ package com.engdiary.mureng.fcm
 
 import android.app.NotificationManager
 import androidx.core.content.ContextCompat
+import com.engdiary.mureng.network.MurengRepository
 import com.engdiary.mureng.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class MurengFCMService : FirebaseMessagingService() {
+
+@AndroidEntryPoint
+class MurengFCMService:FirebaseMessagingService() {
+    @Inject lateinit var ioScope: CoroutineScope
+    @Inject lateinit var murengRepository: MurengRepository
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         sendNotification(remoteMessage)
