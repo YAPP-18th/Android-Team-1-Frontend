c74e5013ce336ad6494a1278e034b9fdb3c2d942
RoadtoS7 <kshvv975@gmail.com>
2021-06-20 18:40:43 +0900

    feature: 마이페이지 View, ViewModel 구현

--- app/src/main/java/com/engdiary/mureng/ui/my/MyPageViewModel.kt
+++ app/src/main/java/com/engdiary/mureng/ui/my/MyPageViewModel.kt
@@ -2,11 +2,13 @@ package com.engdiary.mureng.ui.my
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.engdiary.mureng.data.Author
 import com.engdiary.mureng.network.MurengRepository
 import com.engdiary.mureng.ui.base.BaseViewModel
-import com.engdiary.mureng.ui.social.SocialViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,31 +16,20 @@ class MyPageViewModel @Inject constructor(
     private val murengRepository: MurengRepository
 ) : BaseViewModel(murengRepository) {
 
-    private val _tabItems: MutableLiveData<List<String>> = MutableLiveData()
     private val _position: MutableLiveData<Int> = MutableLiveData()
-    val tabItems: LiveData<List<String>> get() = _tabItems
     val position: LiveData<Int> get() = _position
 
+    private val _user: MutableLiveData<Author> = MutableLiveData()
+    val user: LiveData<Author> get() = _user
 
-    /** 생성자 */
     init {
-        _tabItems.postValue(MyPageViewModel.TAB_ITEMS)
+        initUserInfo()
     }
 
-    fun selectPosition(position: Int) {
-        _position.postValue(position)
-    }
-
-    /** UI 의 onDestroy 개념으로 생각하면 편할듯 */
-    override fun onCleared() {
-        super.onCleared()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    companion object {
-        private val TAB_ITEMS = listOf("기록", "성과", "스크랩")
+    private fun initUserInfo() {
+        viewModelScope.launch(Dispatchers.IO) {
+            murengRepository.getMyInfo()
+                ?.let { _user.postValue(it) }
+        }
     }
 }
