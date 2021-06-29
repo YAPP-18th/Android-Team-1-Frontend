c316336fec2d7af3049d95b76a48b0baf553d6a6
RoadtoS7 <kshvv975@gmail.com>
2021-06-29 10:17:14 +0900

    fix: SplashActivity에서 항상 액세스 토큰과 Refresh Token 갱신하도록 수정

--- app/src/main/java/com/engdiary/mureng/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/engdiary/mureng/ui/splash/SplashViewModel.kt
@@ -1,6 +1,8 @@
 package com.engdiary.mureng.ui.splash
 
 
+import androidx.lifecycle.LiveData
+import com.engdiary.mureng.data.SingleLiveEvent
 import com.engdiary.mureng.network.MurengRepository
 import com.engdiary.mureng.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,8 +10,23 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-        private val murengRepository: MurengRepository
+    private val murengRepository: MurengRepository
 ) : BaseViewModel(murengRepository) {
+    private val _navigateToLogin = SingleLiveEvent<Unit>()
+    val navigateToLogin: LiveData<Unit>
+        get() = _navigateToLogin
+
+    private val _navigateToMain = SingleLiveEvent<Unit>()
+    val navigateToMain: LiveData<Unit>
+        get() = _navigateToMain
+
+    fun postRefreshAccessToken() {
+        murengRepository.postRefreshAccessToken(
+            onSuccess = { _navigateToMain.call() },
+            onFailure = { _navigateToLogin.call() },
+            onError = { _navigateToLogin.call() }
+        )
+    }
 
     /** 생성자 */
     init {
